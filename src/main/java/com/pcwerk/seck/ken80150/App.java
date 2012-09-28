0dbe3a966152a516744f9d699ee00b4455407996
Kai <ken80150@gmail.com>
2012-09-28 10:17:50 -0700

    First Project

---
+++ src/main/java/com/pcwerk/seck/ken80150/App.java
@@ -0,0 +1,13 @@
+package com.pcwerk.seck.ken80150;
+
+/**
+ * Hello There!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello! This is Kai Perng. Nice to see you!" );
+    }
+}
