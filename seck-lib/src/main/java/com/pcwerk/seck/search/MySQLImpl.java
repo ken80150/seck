af4533ea0d34d08decef7c489bc7c6e55996b4b8
Nelson Maltez <subscription@maltez.net>
2012-12-09 18:00:00 -0800

    indexing team, pagerank impl and databasedao implementations
    update seck-lib pom, add hbase and hadoop dependencies.  commit
    DataBaseDao interface changes, HBaseImpl, and PojoImpl implementations.
     add DatabaseFactory to select between database implementations. commit
    PageRank implementation

--- seck-lib/src/main/java/com/pcwerk/seck/search/MySQLImpl.java
+++ seck-lib/src/main/java/com/pcwerk/seck/search/MySQLImpl.java
@@ -2,51 +2,59 @@ package com.pcwerk.seck.search;
 
 import java.util.List;
 
-public class MySQLImpl implements DatabaseDao{
+public class MySQLImpl implements DatabaseDao {
 
-	public void put(String url, String body) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void put(String url, String body) {
+        // TODO Auto-generated method stub
+    }
 
+    public void put(String url, String body, List<String> classifications) {
+        // TODO Auto-generated method stub
+    }
 
-	public void put(String url, String body, List<String> classifications) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void update(String url, String body) {
+        // TODO Auto-generated method stub
+    }
 
+    public void update(String url, List<String> classifications) {
+        // TODO Auto-generated method stub
+    }
 
-	public void update(String url, String body) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void update(String url, String body, List<String> classifications) {
+        // TODO Auto-generated method stub
+    }
 
-	public void update(String url, List<String> classifications) {
-		// TODO Auto-generated method stub
-		
-	}
+    public void delete(String url) {
+        // TODO Auto-generated method stub
+    }
 
+    public String getBody(String url) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	public void update(String url, String body, List<String> classifications) {
-		// TODO Auto-generated method stub
-		
-	}
+    public List<String> getClassications(String url) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
+    /**
+     * Write to MySQL
+     * @param pageRank 
+     */
+    public void put(List<PageRank> pageRank) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-	public void delete(String url) {
-		// TODO Auto-generated method stub
-		
-	}
+    /**
+     * Get from MySQL
+     * @return 
+     */
+    public List<PageRank> getPageRankList() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
-	public String getBody(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-
-	public List<String> getClassications(String url) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
+    public PageRank getPageRank(String docId) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 }
--- seck-lib/src/main/java/com/pcwerk/seck/search/PageRank.java
+++ seck-lib/src/main/java/com/pcwerk/seck/search/PageRank.java
@@ -1,12 +1,60 @@
 package com.pcwerk.seck.search;
 
-import java.util.LinkedHashMap;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-public class PageRank implements RankingMechanism{
+public class PageRank implements Serializable {
 
-	public LinkedHashMap<String, Double> rank(Object o) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    private static final long serialVersionUID = 1L;
+    String id;
+    String url;
+    List<String> linkIns;		//	List of documents' id
+    List<String> linkOuts;		//	List of documents' id
+    float score;
 
+    public PageRank() {
+        linkOuts = new ArrayList<String>();
+        linkIns = new ArrayList<String>();
+    }
+
+    public void setID(String id) {
+        this.id = id;
+    }
+
+    public void setURL(String url) {
+        this.url = url;
+    }
+
+    public void setLinkIns(List<String> linkIns) {
+        this.linkIns.addAll(linkIns);
+    }
+
+    public void setLinkOuts(List<String> linkOuts) {
+        this.linkOuts.addAll(linkOuts);
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public String getID() {
+        return id;
+    }
+
+    public String getURL() {
+        return url;
+    }
+
+    public List<String> getLinkIns() {
+        return linkIns;
+    }
+
+    public List<String> getLinkOuts() {
+        return linkOuts;
+    }
+
+    public float getScore() {
+        return score;
+    }
 }
